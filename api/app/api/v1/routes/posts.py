97d33a4183cf4b742fcdc9ed67e4e4a9f17d784f
rahul-dac-518 <rahulk.scoe.entc@gmail.com>
2026-05-10 09:19:58 +0530

    schema used in post

--- api/app/api/v1/routes/posts.py
+++ api/app/api/v1/routes/posts.py
@@ -3,15 +3,18 @@ from fastapi import (
     UploadFile,
     File,
     Depends,
-    Form
+    Form,
+    HTTPException,
+    status
 )
-
-from app.schemas.post import PostResponse
 
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.dependencies.database import get_db
+
+from app.schemas.post import PostResponse
+
 from app.services.post_service import (
     create_post_service,
     get_post_service
@@ -20,21 +23,30 @@ from app.services.post_service import (
 router = APIRouter()
 
 
-@router.post("/")
+@router.post(
+    "/",
+    response_model=PostResponse,
+    status_code=status.HTTP_201_CREATED
+)
 async def create_post(
     image: UploadFile = File(...),
     caption: str = Form(...),
     db: AsyncSession = Depends(get_db)
 ):
 
-    return await create_post_service(
+    post = await create_post_service(
         image=image,
         caption=caption,
         db=db
     )
 
+    return post
 
-@router.get("/{post_id}")
+
+@router.get(
+    "/{post_id}",
+    response_model=PostResponse
+)
 async def get_post(
     post_id: int,
     db: AsyncSession = Depends(get_db)
@@ -43,17 +55,15 @@ async def get_post(
     post = await get_post_service(post_id, db)
 
     if not post:
-        return {
-            "message": "Post not found"
-        }
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found"
+        )
 
-    return {
-        "image_url": post.image_url,
-        "caption": post.caption
-    }
+    return post
 
 
-@router.get("/")
+@router.get("/health/db")
 async def test_db(
     db: AsyncSession = Depends(get_db)
 ):
--- api/app/schemas/post.py
+++ api/app/schemas/post.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import datetime
+
 
 class PostResponse(BaseModel):
-    id : int
-    image_url : str
-    caption : Optional[str] = None
+
+    id: int
+    image_url: str
+    caption: Optional[str] = None
+    created_at: datetime
 
     class Config:
         from_attributes = True
